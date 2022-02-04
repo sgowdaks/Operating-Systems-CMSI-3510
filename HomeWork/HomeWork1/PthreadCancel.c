07cdb3b1f92e76d1ebf9830bc5f53c47b707fd2b
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-02-04 14:36:23 -0800

    Update PthreadCancel.c

--- HomeWork/HomeWork1/PthreadCancel.c
+++ HomeWork/HomeWork1/PthreadCancel.c
@@ -11,12 +11,20 @@ static void *child(void *ignored) {
 
 int main(int argc, char *argv[]) {
     pthread_t child_thread;
-    char str[1];
+    char str;
 
-    pthread_create(&child_thread, NULL, child, NULL);
+    if(pthread_create(&child_thread, NULL, child, NULL) != 0){
+      printf("Error In creating the thread");
+    }
     printf("Press ENTER to cancel the thread.\n");
-    scanf("%c", str);
-    pthread_cancel(child_thread);
-    printf("The thread has been cancelled.");
+    while(1){
+      str = fgetc(stdin);
+      if(str == 0x0A){
+        pthread_cancel(child_thread);
+          printf("The thread has been cancelled.");
+          break;
+        }
+      }
+    
     return 0;
 }
