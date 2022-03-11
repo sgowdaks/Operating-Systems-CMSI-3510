a92fc39ac41cc3e53fe533954c3ab8fae5db1c74
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-03-11 12:47:47 -0800

    Add files via upload

---
+++ HomeWork/HomeWork2/Boundedbuffer/BoundedBuffer.java
@@ -0,0 +1,31 @@
+public class BoundedBuffer {
+private Object[] buffer = new Object[20];
+private int numOccupied = 0;
+private int firstOccupied = 0;
+
+public synchronized void insert(Object o)
+throws InterruptedException {
+  while(numOccupied == buffer.length) {
+    wait();
+  }
+  buffer[(firstOccupied + numOccupied) % buffer.length] = o;
+  numOccupied++;
+  notifyAll();
+}
+
+
+public synchronized Object retrieve()
+throws InterruptedException
+{
+    while(numOccupied == 0){
+      wait();
+    }
+    Object retrieved = buffer[firstOccupied];
+    buffer[firstOccupied] = null;
+    firstOccupied = (firstOccupied + 1) % buffer.length;
+    numOccupied--;
+    notifyAll();
+  // System.out.printf("Consumer consumes: %d\n",retrieved);
+    return retrieved;
+}
+}
